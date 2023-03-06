58ed760c8d1e2bae772a5b26828af847fc51c1f0
Ramune <ramune.pakalniskiene@gmail.com>
2023-03-06 18:58:24 +0200

    adding calculations mine and teachers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,67 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        // variable:
+        // holder for data
+        // needs good name e.g. myDaughterAge
+        // needs type of variable
+        // that data type limit possible set of allowed values
+        // int - integer numbers
+        int myDaughterAge = 5;
+
+        // assigning new value to variable
+        myDaughterAge = 6;
+        // sout + enter
+        System.out.println(myDaughterAge);
+
+        // use camel case starting with lower letter
+        // String type is for words and letters
+        String myName = "Mariusz";
+        String mySurname = "Pastuszka";
+        System.out.println(myName + "-" + mySurname);
+
+        String five = "5";
+        String otherFive = "5";
+        System.out.println(five + otherFive);
+
+        // true and false
+        boolean trueValue = true;
+        trueValue = false;
+
+        System.out.println(trueValue);
+
+        // 11.0
+        int eleven = 11;
+
+        // double type for number with fraction part
+        double trueEleven = 11.0;
+
+        System.out.println("Exercise 7");
+        int sum = 5 + 8;
+        System.out.println("Sum of 5 and 8 is = " + sum);
+
+        int difference = 11 - 4;
+        System.out.println("Difference of 11 and 4 is: " + difference);
+
+        int product = 5 * 8;
+        System.out.println("Product of 5 and 8 is: " + product);
+
+        double quotient = 5.0/8.5;
+        System.out.println("Quotient of 5.0 and 8.5 is: " + quotient);
+
+        System.out.println("My homework: ");
+
+        double a = 6.0;
+        double b = 3.5;
+        double sum1 = a + b;
+        double difference1 = a - b;
+        double product1 = a * b;
+        double quotient1 = a / b;
+        System.out.println("Sum equals= " + sum1);
+        System.out.println("Difference equals= " + difference1);
+        System.out.println("Product equals= " + product1);
+        System.out.println("Quotient equals= " + quotient1);
+
+
     }
 }
